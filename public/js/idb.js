f2ad0489a6e616761f0a637eff3f98442dd15b19
Lesly <leslyr562@gmail.com>
2022-01-09 19:25:35 -0600

    added service.worker.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -8,7 +8,7 @@ const request = indexedDB.open('budget', 1);
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
+    // create an object store (table) called `new_budget`, set it to have an auto incrementing primary key of sorts 
     db.createObjectStore('new_budget', { autoIncrement: true });
   };
 
@@ -17,7 +17,7 @@ request.onsuccess = function(event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
   
-    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    // check if app is online, if yes run uploadBudget() function to send all local db data to api
     if (navigator.onLine) {
       // we haven't created this yet, but we will soon, so let's comment it out for now
       // uploadBudget();
@@ -34,7 +34,7 @@ function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
     const transaction = db.transaction(['new_budget'], 'readwrite');
   
-    // access the object store for `new_pizza`
+    // access the object store for `new_budget`
     const budgetObjectStore = transaction.objectStore('new_budget');
   
     // add record to your store with add method
