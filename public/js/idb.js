d4681708125b1ad19661d870d8e07445e7689b36
Lesly <leslyr562@gmail.com>
2022-01-09 23:18:18 -0600

    fixed server.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,7 +1,7 @@
 // create variable to hold db connection
 let db;
 // establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
-const request = indexedDB.open('budget', 1);
+const request = indexedDB.open('budget-tracker', 1);
 
 
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
@@ -15,9 +15,7 @@ request.onsuccess = function(event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
   
-    // check if app is online, if yes run uploadBudget() function to send all local db data to api
     if (navigator.onLine) {
-      // we haven't created this yet, but we will soon, so let's comment it out for now
        uploadBudget();
     }
   };
